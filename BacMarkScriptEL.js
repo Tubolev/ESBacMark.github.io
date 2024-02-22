6430933379fb5e3a6bfc7caefb17c0e2652820ad
Tubolev <40029836+Tubolev@users.noreply.github.com>
2024-02-22 09:00:39 +0100

    2nd STS update

--- BacMarkScriptEL.js
+++ BacMarkScriptEL.js
@@ -1,4 +1,4 @@
-var subjects=["L1", "L2", "MA3", "MA5", "EP", "STS", "GE2", "HI2", "PH2", "CHI", "PHY", "BIO", "HI4", "GE4", "AR4", "MU4", "PH4", "L3", "ECO", "LAT", "L4","ONL", "AGR", "L1A", "L2A", "MAA", "MU2", "AR2", "ICT", "IEC", "SOC", "SCP", "L5", "LBI", "LCH", "LPH", "SPO","DRA","ELE"];
+var subjects=["L1", "L2", "MA3", "MA5", "EP", "STS", "GE2", "HI2", "PH2", "CHI", "PHY", "BIO", "HI4", "GE4", "AR4", "MU4", "PH4", "L3", "ECO", "LAT", "L4","ONL", "AGR", "L1A", "L2A", "MAA", "MU2", "AR2", "ICT", "IEC", "SOC", "SCP", "L5", "LBI", "LCH", "LPH", "SPO","DRA","ELE", "ARH"];
 var subPeriods, periods, listOpts;
 var indexes = [];
 var aMarkIDs = [];
@@ -294,7 +294,7 @@ function optErr(){
 	if(optErrors == 0){
 		errorMsg="";
 		document.getElementById("optErrors").innerHTML=errorMsg;
-		printSubjects("L1", "L2", "MA3", "MA5", "EP", "STS", "GE2", "HI2", "PH2", "CHI", "PHY", "BIO", "HI4", "GE4", "AR4", "MU4", "PH4", "L3", "ECO", "LAT", "L4","ONL", "AGR", "L1A", "L2A", "MAA", "MU2", "AR2", "ICT", "IEC", "SOC", "SCP", "L5", "LBI", "LCH", "LPH","SPO", "DRA","ELE");
+		printSubjects("L1", "L2", "MA3", "MA5", "EP", "STS", "GE2", "HI2", "PH2", "CHI", "PHY", "BIO", "HI4", "GE4", "AR4", "MU4", "PH4", "L3", "ECO", "LAT", "L4","ONL", "AGR", "L1A", "L2A", "MAA", "MU2", "AR2", "ICT", "IEC", "SOC", "SCP", "L5", "LBI", "LCH", "LPH","SPO", "DRA","ELE", "ARH");
 	}
 	if(optErrors != 0){
 		errorMsg+="</ol>"
